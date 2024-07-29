e0a3524ecb0d2384dbc36d25a221c6bc9b6a49d2
Mykhailo Shvets <mikhailshvets48@gmail.com>
2024-07-29 13:55:26 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const tree = document.querySelector(".tree");
+const list = [
+    ...document.querySelectorAll("li")
+];
+list.map((li)=>{
+    const span = document.createElement("span");
+    li.prepend(span);
+    span.append(span.nextSibling);
+});
+tree.addEventListener("click", (e)=>{
+    const ul = e.target.nextElementSibling;
+    if (ul.tagName === "UL") ul.hidden = !ul.hidden;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
